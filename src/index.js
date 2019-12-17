2a0685353a54248a7c3d58bfbc6b10c7a133f19e
lijj <13261968767@163.com>
2019-12-17 12:03:26 +0800

    qianfengReact

--- src/index.js
+++ src/index.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component, createContext } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(createContext());
+const { Provider, Consumer: CounterConsumer } = createContext();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class CounterProvider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 100
+    };
+  }
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+  render() {
+    return (
+      <Provider
+        value={{
+          count: this.state.count,
+          onIncrementCount: this.incrementCount,
+          onDecrementCount: this.decrementCount
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+class CountBtn extends Component {
+  render() {
+    return (
+      <CounterConsumer>
+        {({ onIncrementCount, onDecrementCount }) => {
+            const handler = this.props.type === 'increment' ? onIncrementCount : onDecrementCount
+          return <button onClick={handler}>{this.props.children}</button>;
+        }}
+      </CounterConsumer>
+    );
+  }
+}
+class Counter extends Component {
+  render() {
+    return (
+      <CounterConsumer>
+        {({ count }) => {
+          console.log(count);
+          return <span>{count}</span>;
+        }}
+      </CounterConsumer>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <CountBtn type="decrement">-</CountBtn>
+        <Counter />
+        <CountBtn type="increment">+</CountBtn>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CounterProvider>
+    <App />
+  </CounterProvider>,
+  document.querySelector("#root")
+);
