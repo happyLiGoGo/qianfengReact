aa3ed5710dd9b1d6e594eaeea039165e51056d09
lijj <13261968767@163.com>
2019-12-23 09:57:38 +0800

    class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
-// 函数式组件的原理
-// const createApp = props => {
-//   return <h1>welcome {props.title}</h1>;
-// };
-// const App = createApp({
-//   title: "dd"
-// });
-const App = props => {
-  return <h1>welcome {props.title}</h1>;
-};
+import { render } from "react-dom";
 
-ReactDOM.render(<App title="dd" />, document.querySelector("#root"));
+class App extends Component {
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
+// 类组件渲染的原理
+// const app = new App({
+//   title: "dd"
+// }).render();
+// render(app, document.querySelector("#root"));
+render(<App title="dd" />, document.querySelector("#root"));
